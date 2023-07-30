76845933ebe7cc7fbc5c3c135f251acb4385e8e2
Marco <marco.mayorga@gmail.com>
2023-07-29 20:27:37 -0500

    The board is generated in a more react like way.

--- src/Components/Chessboard.jsx
+++ src/Components/Chessboard.jsx
@@ -1,35 +1,37 @@
 import "./Chessboard.css";
 
+// Algebraic chess notation for row and columns like an X and Y axis
 const HorizontalAxis = ["a", "b", "c", "d", "e", "f", "g", "h"];
 const VerticalAxis = ["1", "2", "3", "4", "5", "6", "7", "8"];
 
 export default function Chessboard() {
     let board = [];
 
-    for (let j = VerticalAxis.length - 1; j >= 0; j--) {
-        for (let i = 0; i < HorizontalAxis.length; i++) {
-            const divisor = j + i;
+    // Combines the horizontal and vertical axis in algebraic chess notation in a new array
+    VerticalAxis.toReversed().forEach((boardNumber) => {
+        HorizontalAxis.forEach((boardLetter) => {
+            board.push(boardLetter + boardNumber);
+        });
+    });
 
-            if (divisor % 2 === 0) {
-                board.push(
-                    <div
-                        className="board-piece black-piece"
-                        key={HorizontalAxis[i] + VerticalAxis[j]}>
-                        [{HorizontalAxis[i]}
-                        {VerticalAxis[j]}]
-                    </div>
-                );
-            } else {
-                board.push(
-                    <div
-                        className="board-piece white-piece"
-                        key={HorizontalAxis[i] + VerticalAxis[j]}>
-                        [{HorizontalAxis[i]}
-                        {VerticalAxis[j]}]
-                    </div>
-                );
-            }
-        }
-    }
-    return <div className="board">{board}</div>;
+    // Creates new divs for each element in board array for board peices
+    // also decides color based on index
+    const boardPieces = board.map((boardPiece, index) => {
+        const row = Math.floor(index / 8); // Calculate the row number
+        const col = index % 8; // Calculate the column number
+        console.log(index, row, col);
+
+        // Determine the color of the square based on row and column
+        const isWhiteSquare = (row + col) % 2 === 0;
+        return (
+            <div
+                className={`board-piece ${
+                    isWhiteSquare ? "white-piece" : "black-piece"
+                }`}
+                key={boardPiece}>
+                {boardPiece}
+            </div>
+        );
+    });
+    return <div className="board">{boardPieces}</div>;
 }
